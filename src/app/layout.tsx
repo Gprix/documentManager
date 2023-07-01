7bd92597d6e97bc3d63a92d7648272ab961f4c33
seb-hdz <sebastian.hidalgo@unmsm.edu.pe>
2023-07-01 16:01:56 -0500

    fix(global): split function newAction()

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import "@/styles/globals.css";
-import { Inter } from "next/font/google";
+import { Pathway_Extreme } from "next/font/google";
 import AppProviders from "./providers";
 import useListeners from "@/hooks/useListeners";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
