204cd0754cd2c9569311175127efca559613a07d
seb-hdz <sebastian.hidalgo@unmsm.edu.pe>
2023-07-13 01:52:08 -0500

    style(global): remove unused functions
    + format email service

--- src/services/email/email.service.types.ts
+++ src/services/email/email.service.types.ts
@@ -1,10 +1,9 @@
 export interface EmailOptions {
-    destinatario: string;
-    asunto: string;
-    mensaje: string;
-    remitente: string;
-    contraseña: string;
-  }
-  
-  export type EnviarCorreoFunction = (opciones: EmailOptions) => Promise<void>;
-  
+  destinatario: string;
+  asunto: string;
+  mensaje: string;
+  remitente: string;
+  contraseña: string;
+}
+
+export type EnviarCorreoFunction = (opciones: EmailOptions) => Promise<void>;
