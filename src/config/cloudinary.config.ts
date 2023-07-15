e448e23d8da6207f0a588c578263389b301e033e
gonzalo <gonzalogarciadev@gmail.com>
2023-07-15 02:13:54 -0500

    Minor fix

--- src/config/cloudinary.config.ts
+++ src/config/cloudinary.config.ts
@@ -1,6 +1,5 @@
-import { getEnv } from "@/utils/common.utils";
 
 export const cloudinary = {
-  cloudName: getEnv("NEXT_PUBLIC_REACT_APP_CLOUDINARY_CLOUD_NAME"),
-  uploadPreset: getEnv("NEXT_PUBLIC_REACT_APP_CLOUDINARY_UPLOAD_PRESET"),
+  cloudName: process.env.NEXT_PUBLIC_REACT_APP_CLOUDINARY_CLOUD_NAME,
+  uploadPreset: process.env.NEXT_PUBLIC_REACT_APP_CLOUDINARY_UPLOAD_PRESET,
 };
